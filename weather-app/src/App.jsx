317992e6415dfc791d2a4fd62670a69d724295e2
srijandeep-g <srijandeepghosh2003@gmail.com>
2026-08-16 00:56:25 +0530

    added files

---
+++ weather-app/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import weatherdata from "./assets/weatherdata.js";
+import './App.css'
+
+function App() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+  const getData = () => { 
+    const res = weatherdata.find((item) => 
+      item.city.toLowerCase() === city.toLowerCase()
+    );
+    setWeather(res);
+  }
+  return (
+    <>
+      <div>
+        <h1>Weather App</h1>
+        <div className="container">
+        <input type="text" placeholder="Mumbai"
+          value={city}
+          onChange={(e) => setCity(e.target.value)} />
+        <button onClick={getData}>Search</button>
+        {weather ? (
+          <div className="weather-card">
+            <h2>{weather.city}</h2>
+            <p>Temperature: {weather.temperature} &deg;C</p>
+            <p>Humidity: {weather.humidity}%</p>
+            <p>Condition: {weather.condition}</p>
+          </div>
+        ) : (
+          <p>Enter city and click the button!</p>
+          )}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
+
---
+++ weather-app/src/assets/weatherdata.js
@@ -0,0 +1,103 @@
+const weatherdata = [
+    {
+      "id": 1,
+      "city": "Kolkata",
+      "state": "West Bengal",
+      "temperature": 30,
+      "humidity": 82,
+      "condition": "Cloudy",
+      "windSpeed": 12,
+      "windDirection": "SW"
+    },
+    {
+      "id": 2,
+      "city": "Mumbai",
+      "state": "Maharashtra",
+      "temperature": 28,
+      "humidity": 84,
+      "condition": "Rainy",
+      "windSpeed": 18,
+      "windDirection": "SW"
+    },
+    {
+      "id": 3,
+      "city": "New Delhi",
+      "state": "Delhi",
+      "temperature": 34,
+      "humidity": 58,
+      "condition": "Partly Cloudy",
+      "windSpeed": 10,
+      "windDirection": "NW"
+    },
+    {
+      "id": 4,
+      "city": "Bengaluru",
+      "state": "Karnataka",
+      "temperature": 24,
+      "humidity": 72,
+      "condition": "Cloudy",
+      "windSpeed": 9,
+      "windDirection": "W"
+    },
+    {
+      "id": 5,
+      "city": "Chennai",
+      "state": "Tamil Nadu",
+      "temperature": 31,
+      "humidity": 76,
+      "condition": "Mostly Cloudy",
+      "windSpeed": 15,
+      "windDirection": "S"
+    },
+    {
+      "id": 6,
+      "city": "Hyderabad",
+      "state": "Telangana",
+      "temperature": 29,
+      "humidity": 68,
+      "condition": "Partly Cloudy",
+      "windSpeed": 11,
+      "windDirection": "SW"
+    },
+    {
+      "id": 7,
+      "city": "Pune",
+      "state": "Maharashtra",
+      "temperature": 26,
+      "humidity": 74,
+      "condition": "Rainy",
+      "windSpeed": 14,
+      "windDirection": "W"
+    },
+    {
+      "id": 8,
+      "city": "Ahmedabad",
+      "state": "Gujarat",
+      "temperature": 32,
+      "humidity": 62,
+      "condition": "Sunny",
+      "windSpeed": 13,
+      "windDirection": "W"
+    },
+    {
+      "id": 9,
+      "city": "Jaipur",
+      "state": "Rajasthan",
+      "temperature": 33,
+      "humidity": 55,
+      "condition": "Sunny",
+      "windSpeed": 9,
+      "windDirection": "NW"
+    },
+    {
+      "id": 10,
+      "city": "Lucknow",
+      "state": "Uttar Pradesh",
+      "temperature": 31,
+      "humidity": 70,
+      "condition": "Cloudy",
+      "windSpeed": 8,
+      "windDirection": "E"
+    }
+  ]
+export default weatherdata;
